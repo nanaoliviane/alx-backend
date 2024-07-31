d374d47e769a0fc908341848badec8c839e7d8c2
nanaoliviane <nanaoliviane@gmail.com>
2024-07-31 18:09:03 +0000

    basic flask setup

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -9,8 +9,8 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/', strict_slashes=False)
-def index() -> str:
+@app.route('/')
+def index() 
     '''
     Render the index page
     '''
