1b7ec6a93fcaab48558aa8ead135e148545f3e7a
nanaoliviane <nanaoliviane@gmail.com>
2024-07-31 19:05:18 +0000

    basic flask setup

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -9,18 +9,24 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def index() -> str:
     """
-    Render the index page
+    Render the index page.
 
     Returns:
         str: The rendered template for the index page.
     """
     return render_template('0-index.html')
 
+
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+
+
 
 
 
