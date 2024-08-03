abcde5a829681fdd4d647c77b7a712254ce6ee71
nanaoliviane <nanaoliviane@gmail.com>
2024-08-03 02:23:42 +0000

    babel setup

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,28 +1,22 @@
 #!/usr/bin/env python3
-
+"""A Basic Flask app.
 """
-Basic Flask app setup
-"""
-
 from flask import Flask, render_template
 
 
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 
-@app.route('/', strict_slashes=False)
-def index() -> str:
-    """
-    Render the index page.
-
-    Returns:
-        str: The rendered template for the index page.
+@app.route('/')
+def get_index() -> str:
+    """The home/index page.
     """
     return render_template('0-index.html')
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
 
 
 
